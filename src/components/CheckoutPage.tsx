7f3c26f2c2211b372afe0e7271e6a231880201bd
abhinand-vp <abhinandvpellath@gmail.com>
2024-09-23 23:33:39 +0530

    test

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -47,7 +47,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
       elements,
       clientSecret,
       confirmParams: {
-        return_url: `https://amit-payment-gateway.vercel.app/payment-success?amount=${amount}`,
+        return_url: `${process.env.REACT_APP_BASE_URL}/payment-success?amount=${amount}`,
       },
     });
 
