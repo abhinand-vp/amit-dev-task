84d461d772eba51954acce9d40863d36a82937a1
abhinand-vp <abhinandvpellath@gmail.com>
2024-09-24 09:21:44 +0530

    fails redirection

--- src/app/payment-fails/page.tsx
+++ src/app/payment-fails/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 import React from "react";
 import { FaTimesCircle } from "react-icons/fa";
 
 const PaymentFailed: React.FC = () => {
+  const router = useRouter()
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-br from-red-200 to-red-500">
       <div className="bg-white p-8 rounded-xl shadow-lg text-center">
@@ -21,6 +25,7 @@ const PaymentFailed: React.FC = () => {
 
         <button
           className="bg-red-500 text-white px-6 py-3 rounded-md hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-red-300 transition duration-300"
+          onClick={() => router.push('/')}
         >
           Retry Payment
         </button>
